dd7c6f31288cdcc7243f38f460cb3c805340baf0
Mustafa <mustafa_sahinci@hotmail.com>
2022-05-06 13:55:47 +0000

    finish

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,69 +1,66 @@
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
+    score: 0,
     turnNumber: 0,
     lastButton: "",
     turnInProgress: false,
     choices: ["button1", "button2", "button3", "button4"]
-}
+};
 
 function newGame() {
-    game.score = 0;
-    game.playerMoves = [];
     game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+
     for (let circle of document.getElementsByClassName("circle")) {
         if (circle.getAttribute("data-listener") !== "true") {
-            circle.addEventListener("click", e => {
+            circle.addEventListener("click", (e) => {
                 if (game.currentGame.length > 0 && !game.turnInProgress) {
-                    let move = e.target.getAttribute("id")
-                    game.lastButton = move
-                    lightsOn(move)
-                    game.playerMoves.push(move)
-                    playerTurn()
+                    let move = e.target.getAttribute("id");
+                    game.lastButton = move;
+                    game.playerMoves.push(move);
+                    lightsOn(move);
+                    playerTurn();
                 }
-            })
-            circle.setAttribute("data-listener", "true")
+            });
+            circle.setAttribute("data-listener", "true");
         }
     }
-    showScore()
-    addTurn()
+    showScore();
+    addTurn();
 }
 
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    showTurns()
-}
-
-function showScore() {
-    document.getElementById("score").innerText = game.score
-}
-
-function lightsOn(circ) {
-    document.getElementById(circ).classList.add("light")
-    setTimeout(() => {
-        document.getElementById(circ).classList.remove("light")
-    }, 400);
+    showTurns();
 }
 
 function showTurns() {
     game.turnInProgress = true;
     game.turnNumber = 0;
-    let turns = setInterval(() => {
-        lightsOn(game.currentGame[game.turnNumber])
-        game.turnNumber++
-        if (frameElement.turnNumber >= game.currentGame.length) {
-            clearInterval(turns)
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
             game.turnInProgress = false;
         }
     }, 800);
 }
 
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
 function playerTurn() {
     let i = game.playerMoves.length - 1;
     if (game.currentGame[i] === game.playerMoves[i]) {
-        if (game.currentGame.length == game.playerMoves.length) {
+        if (game.currentGame.length === game.playerMoves.length) {
             game.score++;
             showScore();
             addTurn();
@@ -71,15 +68,11 @@ function playerTurn() {
     } else {
         alert("Wrong move!");
         newGame();
-    };
+    }
 }
 
-module.exports = {
-    game,
-    newGame,
-    showScore,
-    addTurn,
-    lightsOn,
-    showTurns,
-    playerTurn
+function showScore() {
+    document.getElementById("score").innerText = game.score;
 }
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
